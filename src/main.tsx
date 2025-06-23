d20c25469d601b37e65bab01ffc0528b15845368
n8 Smalley <nate.smalley@sentinelone.com>
2025-06-23 15:38:24 -0700

    updated to ensure would run in space

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+if (import.meta.env.MODE === 'development') {
+    localStorage.clear();
+  }
 createRoot(document.getElementById("root")!).render(<App />);
